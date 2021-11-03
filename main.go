e282cf49b0d4bef9f6c46e8b298d54fcfc7dd981
Tatiana <tatyana@devindi.com>
2021-11-03 10:21:52 +0200

    json

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"net/http"
 )
 
+type Relation struct {
+	Id             int
+	DatesLocations map[string][]string
+}
 type API struct {
 	Artists   string
 	Locations string
@@ -26,25 +30,40 @@ type Artist struct {
 	Focations    string
 	ConcertDates string
 	Relations    string
+	Concert      Relation
 }
 
 func main() {
 
 	var links API
-	//var Artists Artist
+	var Artists []Artist
+	// var Relations []Relation
+	//var Relation Relation
 	linkAPI := "https://groupietrackers.herokuapp.com/api"
 	jsonErr := json.Unmarshal(openLink(linkAPI), &links)
 	if jsonErr != nil {
 		log.Fatal(jsonErr)
 	}
-	fmt.Println(links.Artists)
+	//fmt.Println(links.Artists)
 
-	// jsonErr := json.Unmarshal(openLink(string(links.Artists)), &Artists)
-	// if jsonErr != nil {
-	// 	log.Fatal(jsonErr)
-	// }
-	// fmt.Println(links)
+	jsonErr = json.Unmarshal(openLink(links.Artists), &Artists)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	//fmt.Println(Artists)
+	for i, value := range Artists {
+
+		var rel Relation
+		jsonErr = json.Unmarshal(openLink(value.Relations), &rel)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+		}
+		Artists[i].Concert = rel
+	}
+	fmt.Println(Artists)
+
 }
+
 func openLink(linkAPI string) []byte { // read file by http:
 	response, err := http.Get(linkAPI)
 	check(err)
