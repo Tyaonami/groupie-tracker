17effab02d98606ec8e6f61771481ee0b1a4eebe
Tatiana <tatyana@devindi.com>
2021-11-10 00:07:09 +0200

    all

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"strconv"
 	"strings"
 	"text/template"
 )
@@ -58,33 +58,9 @@ func main() {
 func handleRequest() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./templates/static"))))
 	http.HandleFunc("/", index)
-	http.HandleFunc("/artist", showArtist)
 	http.HandleFunc("/404", err404)
 	log.Println("Server running http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
-
-}
-func showArtist(w http.ResponseWriter, r *http.Request) {
-	id, errId := strconv.Atoi(r.URL.Query().Get("id"))
-	tmpl, tmplErr := template.ParseFiles("templates/artist.html", "templates/header.html", "templates/footer.html")
-	if id > len(Artists) || id <= 0 {
-		err = 400
-		errResult = "No Artists info"
-		err404(w, r)
-		return
-	} else if errId != nil {
-		err = 400
-		errResult = "No Artists info"
-		err404(w, r)
-		return
-	} else if tmplErr != nil {
-		err = 404
-		errResult = "This page is not exist"
-		err404(w, r)
-		return
-	} else {
-		tmpl.ExecuteTemplate(w, "artist", Artists[id-1])
-	}
 
 }
 
@@ -95,12 +71,12 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if tmplErr != nil {
 		err = 404
 		errResult = "This page is not exist"
-		err404(w, r)
-		return
-	} else if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	if r.URL.Path != "/" {
 		err = 404
 		errResult = "This page is not exist"
-		err404(w, r)
+		//err404(w, r)
 		return
 	} else {
 		tmpl.ExecuteTemplate(w, "index", Artists)
@@ -115,7 +91,8 @@ func err404(w http.ResponseWriter, r *http.Request) {
 		err = 404
 		errResult = "This page is not exist"
 		w.WriteHeader(http.StatusNotFound)
-	} else if err == 404 {
+	}
+	if err == 404 {
 		w.WriteHeader(http.StatusNotFound)
 	} else if err == 400 {
 		w.WriteHeader(http.StatusBadRequest)
@@ -124,48 +101,6 @@ func err404(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmpl.ExecuteTemplate(w, "404", dataErr)
-
-}
-func getArtis() {
-
-	// var Relations []Relation
-	//var Relation Relation
-	linkAPI := "https://groupietrackers.herokuapp.com/api"
-	jsonErr := json.Unmarshal(openLink(linkAPI), &links)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-	//fmt.Println(links.Artists)
-
-	jsonErr = json.Unmarshal(openLink(links.Artists), &Artists)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-	//fmt.Println(Artists)
-	for i, value := range Artists {
-
-		var rel Relation
-		var rel11 ConcertInfo
-		var rel1 []ConcertInfo
-		jsonErr = json.Unmarshal(openLink(value.Relations), &rel)
-		if jsonErr != nil {
-			log.Fatal(jsonErr)
-		}
-		for k, v := range rel.DatesLocations {
-			i := 0
-			l := strings.ReplaceAll(k, "-", ", ")
-			l = strings.ReplaceAll(l, "_", " ")
-			l = strings.Title(l)
-			rel11.Location = l
-			rel11.Dates = v
-			rel1 = append(rel1, rel11)
-
-			i += 1
-			//fmt.Println(k)
-		}
-		Artists[i].Concert = rel1
-		//fmt.Println(Artists[i].Concert)
-	}
 
 }
 
@@ -207,7 +142,7 @@ func getArtist() {
 			//fmt.Println(k)
 		}
 		Artists[i].Concert = rel1
-		//fmt.Println(Artists[i].Concert)
+		fmt.Println(Artists[i].Concert)
 	}
 
 }
